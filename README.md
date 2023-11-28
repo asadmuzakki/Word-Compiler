9ad10a3119f0747af159065b13bc4bd5673ba5ce
asadmuzakki <asadmuzakki90@gmail.com>
2023-11-28 22:26:16 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Word-Compiler
- 
+# Word Compiler
+#### merupakan sebuah project yang ditujukan untuk menyelesaikan mata kuliah pemrograman berbasis objek. [deskripsi]
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import api.JDBC;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		JDBC runtime = new JDBC();
 	}
 }
